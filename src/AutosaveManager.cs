3a019c65c560180808a117757578ad0ba5981ac9
jake <jbrodsky22@gmail.com>
2019-06-05 00:35:49 -0400

    src: update files

--- src/AutosaveManager.cs
+++ src/AutosaveManager.cs
@@ -9,21 +9,29 @@ namespace AutosaveManager
     {
         private string savePrefix = "";
         private int saveInterval = 0;
+        private bool saveInCombat = true;
         public static Mod mod;
         private string displayTime = "";
 
         private void Start()
         {
             ModSettings settings = mod.GetSettings();
-            saveInterval = settings.GetValue<int>("Section", "AutosaveInterval");
-            savePrefix = settings.GetValue<string>("Section", "AutosavePrefix");
-            InvokeRepeating("Autosave", 1.0f, saveInterval);
+
+            saveInterval = settings.GetValue<int>("Autosave", "AutosaveInterval");
+            savePrefix = settings.GetValue<string>("Autosave", "AutosavePrefix");
+            saveInCombat = settings.GetValue<bool>("Autosave", "SaveInCombat");
+
+            InvokeRepeating("Autosave", saveInterval * 60, saveInterval * 60);
         }
 
         private void Autosave()
         {
-            displayTime = System.DateTime.Now.ToString("HH:mm:ss");
-            GameManager.Instance.SaveLoadManager.Save(GameManager.Instance.PlayerEntity.Name, savePrefix + "-" + displayTime);
+            displayTime = System.DateTime.Now.ToString("HH:mm");
+            var enemiesNearby = GameManager.Instance.AreEnemiesNearby();
+            if( (enemiesNearby && saveInCombat) || !enemiesNearby)
+            {
+                GameManager.Instance.SaveLoadManager.Save(GameManager.Instance.PlayerEntity.Name, savePrefix + "-" + displayTime);
+            }
         }
 
         [Invoke(StateManager.StateTypes.Game, 0)]
