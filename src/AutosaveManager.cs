fb4a40a378aa0d64a447c6348120f531d3c8907e
jake <jbrodsky22@gmail.com>
2019-06-05 22:43:51 -0400

    init

--- src/AutosaveManager.cs
+++ src/AutosaveManager.cs
@@ -1,37 +1,129 @@
 using UnityEngine;
 using DaggerfallWorkshop.Game;
+using DaggerfallWorkshop.Game.UserInterfaceWindows;
 using DaggerfallWorkshop.Game.Utility.ModSupport;
 using DaggerfallWorkshop.Game.Utility.ModSupport.ModSettings;
+
+/**
+ * TODO
+ * remove prefix
+ * autosave limit
+ * infinite autosave
+ * onenterexit
+ * onrest
+ * Use C# DateTime formatting
+ * Tamrielic Calendar
+ * Hour Minute formatting
+ * 24 hour clock
+ * Show Location Name
+ */
+
 
 namespace AutosaveManager
 {
     public class AutosaveManager : MonoBehaviour
     {
-        private string savePrefix = "";
+        // Mod
+        private static Mod mod;
+
+        // General
         private int saveInterval = 0;
-        private bool saveInCombat = true;
-        public static Mod mod;
-        private string displayTime = "";
+        private bool saveInCombat = false;
+        private int autosaveLimit = 0;
+        private bool enableAutosaveTimer = false;
+        private bool infiniteAutosaves = false;
+        private bool onEnterExitSave = false;
+        private bool onRestSave = false;
+
+        // Formatting
+        private string calendarFormat = "";
+        private string timeFormat = "";
+        private bool use24HourClock = false;
+        private bool enableLocation = false;
+
+        // Timer
+        private float lastSaveTime = 0;
+
+        // FD save name
+        private string saveName = "";
 
         private void Start()
         {
             ModSettings settings = mod.GetSettings();
 
+            // General
             saveInterval = settings.GetValue<int>("Autosave", "AutosaveInterval");
-            savePrefix = settings.GetValue<string>("Autosave", "AutosavePrefix");
+            enableAutosaveTimer = saveInterval == 0 ? true : false;
             saveInCombat = settings.GetValue<bool>("Autosave", "SaveInCombat");
+            autosaveLimit = settings.GetValue<int>("Autosave", "AutosaveLimit");
+            infiniteAutosaves = settings.GetValue<bool>("Autosave", "InfiniteAutosaves");
+            onEnterExitSave = settings.GetValue<bool>("Autosave", "OnEnterExitSave");
+            onRestSave = settings.GetValue<bool>("Autosave", "OnRestSave");
 
-            InvokeRepeating("Autosave", saveInterval * 60, saveInterval * 60);
+            // Formatting
+            calendarFormat = settings.GetValue<string>("Formatting", "Calendar");
+            timeFormat = settings.GetValue<string>("Formatting", "Time");
+            use24HourClock = settings.GetValue<bool>("Formatting", "24hrClock");
+            enableLocation = settings.GetValue<bool>("Formatting", "ShowLocationName");
+
+            saveName = "Autosave -";
+
+            if (enableAutosaveTimer)
+            {
+                Debug.Log("enableAutosaveTimer");
+
+                // Check the watch every 45 seconds
+                InvokeRepeating("CheckWatch", 45, 45);
+            }
+            if (onRestSave)
+            {
+                Debug.Log("onRestSave");
+
+                // Subscribe to sleep event
+                DaggerfallRestWindow.OnSleepTick += Autosave;
+            }
+            if (onEnterExitSave)
+            {
+                Debug.Log("onEnterExitSave");
+
+                // Subscribe to transition events
+                PlayerEnterExit.OnTransitionExterior += FireTransitionSave;
+                PlayerEnterExit.OnTransitionInterior += FireTransitionSave;
+                PlayerEnterExit.OnTransitionDungeonExterior += FireTransitionSave;
+                PlayerEnterExit.OnTransitionDungeonInterior += FireTransitionSave;
+            }
         }
 
         private void Autosave()
         {
-            displayTime = System.DateTime.Now.ToString("HH:mm");
+            Debug.Log("Autosave");
             var enemiesNearby = GameManager.Instance.AreEnemiesNearby();
             if( (enemiesNearby && saveInCombat) || !enemiesNearby)
             {
-                GameManager.Instance.SaveLoadManager.Save(GameManager.Instance.PlayerEntity.Name, savePrefix + "-" + displayTime);
+                saveName += System.DateTime.Now.ToString(timeFormat + calendarFormat);
+                if (enableLocation)
+                {
+                    saveName += " " + GameManager.Instance.PlayerGPS.CurrentLocation.Name;
+                }
+                lastSaveTime = Time.realtimeSinceStartup;
+                GameManager.Instance.SaveLoadManager.Save(GameManager.Instance.PlayerEntity.Name, saveName);
             }
+        }
+
+        private void CheckWatch()
+        {
+            Debug.Log("Check Watch");
+            float timeNow = Time.realtimeSinceStartup;
+            if (!InputManager.Instance.IsPaused && timeNow - lastSaveTime > saveInterval * 60)
+            {
+                Autosave();
+            }
+        }
+
+        private void FireTransitionSave(PlayerEnterExit.TransitionEventArgs args)
+        {
+            Debug.Log("FireTransitionSave");
+            Autosave();
         }
 
         [Invoke(StateManager.StateTypes.Game, 0)]
